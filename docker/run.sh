7860cdef885f5920663e4ad727b2d2ad3b46ff76
Shaun Einolf <seinolf@gmail.com>
2023-09-08 14:39:31 -0400

    adding site_path variable to make run and dockerfile more flexible

--- docker/run.sh
+++ docker/run.sh
@@ -1,6 +1,5 @@
-home_dir='/var/www/drupal'
-cd $home_dir
-FILE=$home_dir/installed.txt
+cd $site_path
+FILE=$site_path/installed.txt
 if [ -f "$FILE" ]; then
     echo "Drupal Site Already Installed"
 else
@@ -10,23 +9,23 @@ else
     #mysql -u$db_user -p$db_password -h$db_host -e "create database $db_name;"
     #drush site:install -y standard --db-url=mysql://$db_user:$encoded_password@$db_host:$db_port/$db_name --account-name=$account_name --account-pass=$account_pass --site-name=$sitename
     touch installed.txt
-    cp $home_dir/web/sites/default/default.settings.php $home_dir/web/sites/default/settings.php
-    sed -i '/$settings\['"'"'hash_salt'"'"'\] = '"'"''"'"';/d' $home_dir/web/sites/default/settings.php
+    cp $site_path/web/sites/default/default.settings.php $site_path/web/sites/default/settings.php
+    sed -i '/$settings\['"'"'hash_salt'"'"'\] = '"'"''"'"';/d' $site_path/web/sites/default/settings.php
 
-    cat $home_dir/settings.php.patch >> $home_dir/web/sites/default/settings.php
+    cat $site_path/settings.php.patch >> $site_path/web/sites/default/settings.php
     composer update
     drush en -y content_sync book pathauto migrate migrate_drupal migrate_drupal_ui backup_migrate migrate_plus migrate_upgrade
     if [ -d "$homedir/files" ]; then
-       cp -R $home_dir/files/files/* $home_dir/web/sites/default/files
-       rm -rf $home_dir/files/private
-       cp -R $home_dir/files/files-private $home_dir/web/sites/default/files/private
+       cp -R $site_path/files/files/* $site_path/web/sites/default/files
+       rm -rf $site_path/files/private
+       cp -R $site_path/files/files-private $site_path/web/sites/default/files/private
     fi
-    chown -R apache:apache $home_dir
+    chown -R apache:apache $site_path
     drush ucrt admin
     drush urol administrator admin
     drush upwd admin 1234
-    rm -rf $home_dir/content/sync/entities
-    rm -rf $home_dir/content/sync/files
+    rm -rf $site_path/content/sync/entities
+    rm -rf $site_path/content/sync/files
 fi
 crond
 exec httpd -DFOREGROUND
