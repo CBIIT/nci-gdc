45ffc020774526f9593750a889a6272b09df1fed
Shaun Einolf <seinolf@gmail.com>
2023-09-12 11:30:00 -0400

    adding build script

---
+++ docker/build.sh
@@ -0,0 +1,4 @@
+docker rm -f chrisimage
+docker build -t chrisimage .
+
+docker run -v $CONTENT_SYNC:/var/www/drupal/content_sync -v $FILES:/var/www/drupal/files -p 8098:80 -e db_user=$DB_USER -e db_password=$DB_PASSWORD -e db_host=$DB_HOST -e db_name=chris -e db_port=$DB_PORT -e hash_salt=hashsalt -d --name chrisimage
