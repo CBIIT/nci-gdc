03b86dc6a77f47da1c468f07886b0f4f4314dfb3
Shaun Einolf <seinolf@gmail.com>
2023-09-19 11:50:35 -0400

    adding entity_embed

--- docker/run.sh
+++ docker/run.sh
@@ -3,34 +3,45 @@ FILE=$site_path/installed.txt
 if [ -f "$FILE" ]; then
     echo "Drupal Site Already Installed"
 else
-    #encoded_password=$(python3 -c "import urllib.parse; print(urllib.parse.quote('$db_password'))")
+    encoded_password=$(python3 -c "import urllib.parse; print(urllib.parse.quote('$db_password'))")
 
-    #mysql -u$db_user -p$db_password -h$db_host -e "drop database IF EXISTS $db_name;"
-    #mysql -u$db_user -p$db_password -h$db_host -e "create database $db_name;"
-    #drush site:install -y standard --db-url=mysql://$db_user:$encoded_password@$db_host:$db_port/$db_name --account-name=$account_name --account-pass=$account_pass --site-name=$sitename
+    mysql -u$db_user -p$db_password -h$db_host -e "drop database IF EXISTS $db_name;"
+    mysql -u$db_user -p$db_password -h$db_host -e "create database $db_name;"
+    drush site:install -y standard --db-url=mysql://$db_user:$encoded_password@$db_host:$db_port/$db_name --account-name=$account_name --account-pass=$account_pass --site-name=$sitename
     touch installed.txt
-    cp $site_path/web/sites/default/default.settings.php $site_path/web/sites/default/settings.php
     cat $site_path/settings.php.patch >> $site_path/web/sites/default/settings.php
+
+    composer require drupal/entity_embed:^1.4 drupal/markdown erusev/parsedown --no-update
+    #drush y:get:value contentsync/filter.format.markdown.yml uuid
+    echo "need to add markdown content authoring type, then after upgrade save the format again"
     composer update
-    drush en -y markdown content_sync book pathauto migrate migrate_drupal migrate_drupal_ui backup_migrate migrate_plus migrate_upgrade
-    if [ -d "$homedir/files" ]; then
-       mkdir $site_path/web/sites/default/files
-       cp -R $site_path/files/files/* $site_path/web/sites/default/files
+    drush en -y markdown content_sync book entity_embed pathauto migrate migrate_drupal migrate_drupal_ui backup_migrate migrate_plus migrate_upgrade markdown
+    drush cim -y --source=$site_path/config/sync --partial
+
+    if [ -d "/tmp/files" ]; then
+       cp -R /tmp/files $site_path
        rm -rf $site_path/files/private
-       mkdir $site_path/web/sites/default/files/private
-       cp -R $site_path/files/files-private/* $site_path/web/sites/default/files/private
+       cp -R $site_path/files/files-private $site_path/files/private
     fi
     chown -R apache:apache $site_path/web
+    python3 db_fix.py $migrate_db_name $db_user $db_password $db_host $db_port
+    drush migrate-upgrade --legacy-db-key='migrate'  --legacy-root='$site_path/files'
     drush ucrt admin
     drush urol administrator admin
     drush upwd admin 1234
     rm -rf $site_path/content/sync/entities
     rm -rf $site_path/content/sync/files
+    drush cdel -y block.block.bartik_system_main
+    drush cdel -y block.block.bartik_system_powered_by
+    drush cdel -y block.block.cag_bootstrap_system_main
     drush cim -y --source=$site_path/config/sync --partial
-    drush thin gdc_foundation
-    drush thin zurb_foundation
-    drush config-set -y system.theme default gdc_foundation
-    drush cr
+
+    drush cse -y
+    #drush cex?
+    tar cvf $site_path/contentsync/content.tgz $site_path/content/sync/*
+    ## changing ownership to 3000 which is drupaldocker user ##
+    chown -R 3000:3000 $site_path/contentsync
+    echo "need to add markdown content authoring type, then after upgrade save the format again"
+
 fi
-crond
 exec httpd -DFOREGROUND
