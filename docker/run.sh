d9eca7da3e1013ae6ef7f7aa7bc1c5a35fc1d953
Shaun Einolf <seinolf@gmail.com>
2024-01-25 09:38:55 -0500

    adding /docker to codepath

--- docker/run.sh
+++ docker/run.sh
@@ -4,11 +4,11 @@ if [ -f "$FILE" ]; then
     echo "Drupal Site Already Installed"
 else
     touch installed.txt
-    cp $code_path/composer.lock ./ 
-    cp -r $code_path/web ./
+    cp $code_path/docker/composer.lock ./ 
+    cp -r $code_path/docker/web ./
     composer install
     cp web/sites/default/default.settings.php web/sites/default/settings.php
-    cat $code_path/settings.php.patch >> web/sites/default/settings.php
+    cat $code_path/docker/settings.php.patch >> web/sites/default/settings.php
     mv content_sync web/modules/contrib
     chown -R www-data:www-data web
     echo "changing ownership of web to www-data"
