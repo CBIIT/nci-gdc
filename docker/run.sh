a68faf0466f64cb3845f10ea263e753bb954d1dc
Shaun Einolf <seinolf@gmail.com>
2024-02-07 21:28:22 -0500

    adding htacess, modifying run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -7,21 +7,19 @@ else
     cp $code_path/docker/composer.lock ./ 
     cp $code_path/docker/composer.json ./
     cp -r $code_path/docker/web ./
-    cp $code_path/docker/htaccess.patch web
     composer config --no-plugins allow-plugins.cweagans/composer-patches true
     composer install
     cp web/sites/default/default.settings.php web/sites/default/settings.php
     cat $code_path/docker/settings.php.patch >> web/sites/default/settings.php
-
     mv content_sync web/modules/contrib
     chown -R www-data:www-data web
     echo "changing ownership of web to www-data"
     drush cr
 fi
 cat web/sites/default/settings.php
+cp $code_path/docker/.htaccess web
 echo $s3_access_key
 echo $s3_secret_key
-patch web/.htaccess web/htaccess.patch
 echo "starting apache"
 drush cr
 apache2-foreground
