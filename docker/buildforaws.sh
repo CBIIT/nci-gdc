50c6fac132e7ae9c9f9de19975e4d314910adde7
Shaun Einolf <seinolf@gmail.com>
2024-02-13 19:38:31 -0500

    build for aws

--- docker/buildforaws.sh
+++ docker/buildforaws.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # add the image name/tag as paramater ./buildforaws.sh imagename:tag #
-awsdockerlogin
+aws ecr get-login-password | docker login -u AWS --password-stdin "https://$(aws sts get-caller-identity --query 'Account' --output text).dkr.ecr.$(aws configure get region).amazonaws.com"
 docker build --no-cache --platform linux/amd64 -t $1 .
 docker push $1
