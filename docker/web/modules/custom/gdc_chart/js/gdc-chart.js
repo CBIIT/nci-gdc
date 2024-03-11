17173a4303b58113e8128895a70512408d85e8a4
Chris Kneisler <cpkneisler@gmail.com>
2024-03-11 18:08:20 +0000

    more efficient gdc chart js code

--- docker/web/modules/custom/gdc_chart/js/gdc-chart.js
+++ docker/web/modules/custom/gdc_chart/js/gdc-chart.js
@@ -1,28 +1,27 @@
-/**                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
- * @file                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
- * gdc_chart behaviors.                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
- */
-
-
-
-var humanBodyData = {};
-var dataResponse = [];
- 
-var chart;
-
-
 (function($, Drupal) {
+    let humanBodyData = {};
+    let dataResponse = [];
+    let chart;
+
     Drupal.behaviors.gdcChartGdcChart = {
         attach: function(context, settings) {
             once('gdcChartGdcChart', 'html').forEach(function(element) {
                 $(document).ready(function() {
-                    getProjects();
-                    displayChart();
-                    getHumanBodyData();
+                    initialize();
                 });
             });
         },
     };
+
+    async function initialize() {
+        try {
+            await getProjects();
+            await getHumanBodyData();
+            displayChart();
+        } catch (error) {
+            console.error('Initialization error:', error);
+        }
+    }
 
     function displayChart() {
         const ctx = document.getElementById('gdcChart');
@@ -31,14 +30,17 @@ var chart;
             data: {
                 labels: dataResponse.map(item => item.id),
                 datasets: [{
-                data: dataResponse.map(item => item.cases),
-                borderWidth: 1,
-                backgroundColor: ["#F0EE37", "#9E09DB", "#DF2EBD", "#F0773B", "#26C156", "#1f77b4", "#44D52D", "#1799E3", "#C736CF", "#E04B7B", "#1B72F6", "#3CA6CD", "#DEAF35", "#43DDCF", "red", "#F52AB6", "#69A1EC", "#BE302C", "#F5619A", "#348522", "#D7215D", "#DE3DD3", "#D7BC58", "#DA7D3A", "#9C9C9C", "#F08E1A", "#3DDFCC", "#E4DE3E", "#E96E1F", "#D58D00"],
-                hoverBorderColor: '#000',
-                hoverBorderWidth: 3,
+                    data: dataResponse.map(item => item.cases),
+                    borderWidth: 1,
+                    backgroundColor: ["#F0EE37", "#9E09DB", "#DF2EBD", "#F0773B", "#26C156", "#1f77b4", "#44D52D", "#1799E3", "#C736CF", "#E04B7B", "#1B72F6", "#3CA6CD", "#DEAF35", "#43DDCF", "red", "#F52AB6", "#69A1EC", "#BE302C", "#F5619A", "#348522", "#D7215D", "#DE3DD3", "#D7BC58", "#DA7D3A", "#9C9C9C", "#F08E1A", "#3DDFCC", "#E4DE3E", "#E96E1F", "#D58D00"],
+                    hoverBorderColor: '#000',
+                    hoverBorderWidth: 3,
                 }],
             },
             options: {
+                animation: {
+                    animateScale:true
+                },
                 onClick: function(event, elements) {
                     if (elements.length > 0) {
                         var url = 'https://portal.gdc.cancer.gov/';
@@ -47,9 +49,9 @@ var chart;
                 },
                 onHover: function(event, elements) {
                     if (elements && elements.length) {
-                        document.getElementById('gdcChart').style.cursor = 'pointer';
+                        ctx.style.cursor = 'pointer';
                     } else {
-                        document.getElementById('gdcChart').style.cursor = 'default';
+                        ctx.style.cursor = 'default';
                     }
                 },
                 plugins: {
@@ -60,9 +62,7 @@ var chart;
                         callbacks: {
                             label: function(context, data) {
                                 var currentSite = dataResponse[context.dataIndex];
-                                var index = context.index;
-                                label = ` ${currentSite['cases'].toLocaleString()} cases ( ${currentSite['files'].toLocaleString()} Files)`;
-                                return label;
+                                return ` ${currentSite['cases'].toLocaleString()} cases (${currentSite['files'].toLocaleString()} Files)`;
                             }
                         }
                     }
@@ -71,112 +71,101 @@ var chart;
         });
     }
 
-    function getHumanBodyData() {
-        $.get( "/chart/diseasedata.json", function() {
-        })
-        .done(function(data) {
-            getCaseData().then(caseData => {
-                let cases = {};
-                for (let key in data) {
-                    cases[key] = [0, 0];
-                    const siteArray = data[key]['byPrimarySite'];
-                    siteArray.forEach(siteItem => {
-                        caseData.forEach(caseItem => {
-                            if (caseItem['key'].toLowerCase() === siteItem.toLowerCase()) {
-                                cases[key][0] += parseInt(caseItem['doc_count']);
-                                cases[key][1] += parseInt(caseItem['files']);
-                            }
-                        });
-                    });
-                }
-                dataResponse = Object.entries(cases).map(([id, [cases, files]]) => ({
-                    id,
-                    cases,
-                    files
-                }));
-                let newData = dataResponse.map(item => item.cases);
-                chart.data.datasets[0].data = newData;
-                chart.update();
-            }).catch(error => {
-                console.error('There was a problem fetching the case data:', error);
-            });            
-        })
-        .fail(function() {
-            console.log('error');
-        });
+    async function getHumanBodyData() {
+        try {
+            const data = await $.get("/chart/diseasedata.json");
+            const caseData = await getCaseData();
+            processData(data, caseData);
+        } catch (error) {
+            console.error('Error fetching human body data:', error);
+        }
     }
 
+    function processData(data, caseData) {
+        let cases = {};
+        for (let key in data) {
+            cases[key] = [0, 0];
+            const siteArray = data[key]['byPrimarySite'];
+            siteArray.forEach(siteItem => {
+                caseData.forEach(caseItem => {
+                    if (caseItem['key'].toLowerCase() === siteItem.toLowerCase()) {
+                        cases[key][0] += parseInt(caseItem['doc_count']);
+                        cases[key][1] += parseInt(caseItem['files']);
+                    }
+                });
+            });
+        }
+        dataResponse = Object.entries(cases).map(([id, [cases, files]]) => ({
+            id,
+            cases,
+            files
+        }));
+        let newData = dataResponse.map(item => item.cases);
+        chart.data.datasets[0].data = newData;
+        chart.update();
+    }
 
-    function getCaseData() {                                                                                                                                                                                                                    
-        const url = 'https://api.gdc.cancer.gov/v0/graphql/HumanBody?hash=f17e72f7b92417006e341859e334b4fc';                                                                                                                                    
-        const body = {                                                                                                                                                                                                                          
-            "query":"query HumanBody_relayQuery {\nviewer {\nrepository {\nfiles {\naggregations {\ncases__primary_site {\nbuckets {\ndoc_count\nkey\n}\n}\n}\n}\ncases {\naggregations {\nprimary_site {\nbuckets {\ndoc_count\nkey\n}\n}\n}\n}\n}\n}\n}\n","variables":{}                                                                            
-        };                                                                                                   
-                                                                                                             
-        // Return a promise to properly handle the asynchronous nature of the AJAX request                   
-        return new Promise(function(resolve, reject) {                                                       
-            $.post(url, body, function(response) {                                                           
-                const cases = {};                                                                            
-                const primarySites = response.data.viewer.repository.cases.aggregations.primary_site.buckets;
-                const files = response.data.viewer.repository.files.aggregations.cases__primary_site.buckets;
-                                                                                         
-                // Create a map of file counts by key                                    
-                const fileCountsByKey = {};                                              
-                files.forEach(file => {                                                    
-                    fileCountsByKey[file.key] = file.doc_count;                            
-                });                                                                                                 
-                                                                                                                    
-                // Iterate over primary sites to populate the cases object                                          
-                primarySites.forEach(primarySite => {                                                               
-                    const key = primarySite.key;                                                                    
-                    const docCount = primarySite.doc_count;                                                         
-                    const filesCount = fileCountsByKey[key] || 0;                                                   
-                                                                                                                                                                   
-                    cases[key] = [docCount, filesCount];                                                                                                           
-                });                                                                                                                                                
-                                                                                                                                                                   
-                // Convert the cases object to an array of objects                                                                                                 
-                const result = Object.entries(cases).map(([id, [doc_count, files]]) => ({                                                                          
-                    key: id,                                                                                                                                       
-                    doc_count: doc_count,                                                                                                                          
-                    files: files                                                                                                                                   
-                }));                                                                                                                                                                                                                            
-                                                                                                                                                                   
-                resolve(result); // Resolve the promise with the result                                                                                            
-            }).fail(function() {                                                                                                                                   
-                reject(new Error('There was a problem fetching the case data')); // Reject the promise with an error                                               
-            });                                                                                                                                                    
-        });                                                                                                                                                        
-    } 
-    
+    async function getCaseData() {
+        const url = 'https://api.gdc.cancer.gov/v0/graphql/HumanBody?hash=f17e72f7b92417006e341859e334b4fc';
+        const body = {
+            "query": "query HumanBody_relayQuery {\nviewer {\nrepository {\nfiles {\naggregations {\ncases__primary_site {\nbuckets {\ndoc_count\nkey\n}\n}\n}\n}\ncases {\naggregations {\nprimary_site {\nbuckets {\ndoc_count\nkey\n}\n}\n}\n}\n}\n}\n}",
+            "variables": {}
+        };
 
-    function getProjects() {
+        try {
+            const response = await $.post(url, body);
+            const cases = {};
+            const primarySites = response.data.viewer.repository.cases.aggregations.primary_site.buckets;
+            const files = response.data.viewer.repository.files.aggregations.cases__primary_site.buckets;
+
+            const fileCountsByKey = {};
+            files.forEach(file => {
+                fileCountsByKey[file.key] = file.doc_count;
+            });
+
+            primarySites.forEach(primarySite => {
+                const key = primarySite.key;
+                const docCount = primarySite.doc_count;
+                const filesCount = fileCountsByKey[key] || 0;
+
+                cases[key] = [docCount, filesCount];
+            });
+
+            const result = Object.entries(cases).map(([id, [doc_count, files]]) => ({
+                key: id,
+                doc_count: doc_count,
+                files: files
+            }));
+
+            return result;
+        } catch (error) {
+            throw new Error('There was a problem fetching the case data');
+        }
+    }
+
+    async function getProjects() {
         const humanbody_url = 'https://api.gdc.cancer.gov/projects?facets=project_id,primary_site,disease_type,program.name,summary.case_count,summary.file_count';
-        fetch(humanbody_url, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        })
-        .then(response => {
+        try {
+            const response = await fetch(humanbody_url, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            });
             if (!response.ok) {
                 throw new Error('Network response was not ok');
             }
-            return response.json();
-        })
-        .then(data => {
+            const data = await response.json();
             let aggregations = data.data.aggregations;
             $('#diseaseTypes')[0].innerHTML = aggregations.disease_type.buckets.length.toLocaleString();
             $('#primarySites')[0].innerHTML = aggregations.primary_site.buckets.length.toLocaleString();
             $('#projects')[0].innerHTML = aggregations.project_id.buckets.length.toLocaleString();
             $('#cases')[0].innerHTML = aggregations['summary.case_count']['stats']['sum'].toLocaleString();
             $('#programs')[0].innerHTML = aggregations['program.name']['buckets'].length.toLocaleString();
-        })
-        .catch(error => {
+        } catch (error) {
             console.error('There was a problem fetching the human body data:', error);
-        });
+        }
     }
 
-
-
 })(jQuery, Drupal);
+
