c780dabec9cf9718ed933e9c0ddd1b58fdc6d673
Shaun Einolf <seinolf@gmail.com>
2024-03-18 12:31:04 -0400

    adding postfix

--- docker/run.sh
+++ docker/run.sh
@@ -11,6 +11,7 @@ else
     composer config --no-plugins allow-plugins.cweagans/composer-patches true
     composer install
     cp web/sites/default/default.settings.php web/sites/default/settings.php
+    echo "relayhost = [mailfwd.nih.gov]" > /etc/postfix/main.cf
     cat $code_path/docker/settings.php.patch >> web/sites/default/settings.php
     git clone https://git.drupalcode.org/issue/content_sync-3330173.git $drupal_root/web/modules/contrib/content_sync
     git -C $drupal_root/web/modules/contrib/content_sync checkout 3330173-D10-compatibility-beta    
@@ -18,7 +19,7 @@ else
     echo "changing ownership of web to www-data"
     drush cr
 fi
-cat web/sites/default/settings.php  
 echo "starting apache"
 drush cr
+postfix start
 httpd -D FOREGROUND
