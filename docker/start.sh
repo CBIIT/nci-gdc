3073e3ee4a25f02073e08714c913ee3d39b5fb75
Shaun Einolf <seinolf@gmail.com>
2024-03-22 12:33:59 -0400

    removing webform libraries download, moving apache ownership change to installscript

--- docker/start.sh
+++ docker/start.sh
@@ -1,14 +1,3 @@
 #!/bin/sh
-FILE=installed.txt
-if [ -f "$FILE" ]; then
-    echo "Drupal Site Already Installed"
-else
-    touch installed.txt
-    drush webform-libraries-download
-    chown -R apache:apache web
-    echo "changing ownership of web to www-data"
-    drush cr
-fi
-drush cr
 postfix start
 httpd -D FOREGROUND
