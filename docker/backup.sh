4da6ef87e6271ef7d305c21d002affb078523b37
Chris Kneisler <cpkneisler@gmail.com>
2024-04-03 20:51:28 +0000

    updating backup script to not backup db

--- docker/backup.sh
+++ docker/backup.sh
@@ -8,10 +8,10 @@ echo "*** Set maintenance mode ON"
 #drush state:set system.maintenance_mode 1 --input-format=integer
 echo "drush cr"
 drush cr
-echo "*** Backuping up Druapl DB into database.sql"
-drush sql-dump --ordered-dump > database.sql
-tar cfz database.sql.tgz database.sql
-echo "Backup is complete"
+#echo "*** Backuping up Druapl DB into database.sql"
+#drush sql-dump --ordered-dump > database.sql
+#tar cfz database.sql.tgz database.sql
+#echo "Backup is complete"
 echo "*** Set maintencance mode OFF "
 drush state:set system.maintenance_mode 0 --input-format=integer
 echo "drush cr"
